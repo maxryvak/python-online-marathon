0ab68c0cb8aefdd2c5ab1576792e842e8c8df7a3
maxryvak <114568021+maxryvak@users.noreply.github.com>
2022-10-13 22:02:46 +0300

    Update task-1.py

--- sprint-05/task-1.py
+++ sprint-05/task-1.py
@@ -1,9 +1,28 @@
-def check_number_group(number):
+def check_number_group_1(number):
     try:
         if int(number) <= 10:
             return "We obtain error:Number of your group can't be less than 10"
         else:
             return f"Number of your group {number} is valid"
 
+    except ValueError:
+        return "You entered incorrect data. Please try again."
+    
+
+    
+    
+class ToSmallNumberGroupError(Exception): 
+     def __init__(self, msg):
+        self.msg = msg
+    
+# enter your code 
+def check_number_group_2(number):
+    try:
+        if int(number) <= 10:
+            raise (ToSmallNumberGroupError("We obtain error:Number of your group can't be less than 10"))
+        else:
+            return f"Number of your group {number} is valid"
+    except ToSmallNumberGroupError as error:
+        return error.msg
     except ValueError:
         return "You entered incorrect data. Please try again."
