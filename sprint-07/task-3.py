1550b964a4b06cfedb0f399df4bd8ba2318d800c
maxryvak <114568021+maxryvak@users.noreply.github.com>
2022-10-20 12:00:51 +0300

    Create task-3.py

---
+++ sprint-07/task-3.py
@@ -0,0 +1,45 @@
+class MotorCycle:
+    """Class for MotorCycle"""
+
+    def __init__(self):
+        self.name = "MotorCycle"
+
+    def TwoWheeler(self):
+        return "TwoWheeler"
+
+
+class Truck:
+    def __init__(self):
+        self.name = "Truck"
+
+    def EightWheeler(self):
+        return "EightWheeler"
+
+class Car:
+    def __init__(self):
+        self.name = "Car"
+
+    def FourWheeler(self):
+        return "FourWheeler"
+
+class Adapter:
+    """
+    Adapts an object by replacing methods.
+    Usage:
+    motorCycle = MotorCycle()
+    motorCycle = Adapter(motorCycle, wheels = motorCycle.TwoWheeler)
+    """
+
+    def __init__(self, obj, **adapted_methods):
+        """We set the adapted methods in the object's dict"""
+        self.obj = obj
+        self.__dict__.update(adapted_methods)
+
+    def __getattr__(self, attr):
+        """All non-adapted calls are passed to the object"""
+        return getattr(self.obj, attr)
+
+    def original_dict(self):
+        """Print original object dict"""
+        return self.obj.__dict__
+  
